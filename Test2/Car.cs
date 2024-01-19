7d63172c0542d398434063bfc089bf4cf7050642
Volodymyr Vasylkiv <heavyguitarriff@gmail.com>
2024-01-19 10:48:18 +0200

    Add project files.

---
+++ Test2/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    internal class Car
+    {
+        public int vin;
+        public string brand;
+        public string model;
+
+        public Car(int vin2, string brand2, string model2) {
+            this.vin = vin2;
+            this.brand = brand2;
+            this.model = model2;
+        }
+
+
+        public Car() { }
+
+        public Car(string brand, string model) { 
+
+            this.brand = brand;
+            this.model = model;
+        }
+
+
+    }
+}
+
---
+++ Test2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Test2
+{
+    class Program
+
+    {
+
+
+        public static void Main(string[] args)
+        {
+
+
+            Car car1 = new Car(123, "Tesla", "Model S");
+
+            Car car2;
+            car2 = new Car(456, "Tesla", "Model X");
+
+            Car car3 = new Car();
+
+            car3.vin = 555;
+            car3.brand = "BMW";
+            car3.model = "7";
+
+
+            Car car4 = new Car("Audi", "Q8");
+            car4.vin = 001;
+
+
+            Car[] cars = { car1, car2, car3 };
+
+
+            int i = 1;
+
+            foreach (Car car in cars) {
+
+               
+
+                Console.WriteLine("Car Number: " + i);
+                Console.WriteLine("VIN CODE: " + car.vin);
+                Console.WriteLine("Brand: " + car.brand);
+                Console.WriteLine("Model: " + car.model);
+                i++;
+
+
+                Console.WriteLine("_______________");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+
+
+}
+
+    
